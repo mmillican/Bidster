f73af784c2bf3958cb9f492bb0bd526ca2060186
Matt Millican <matt@mattmillican.com>
2019-02-05 23:17:36 -0600

    Add bidding summary to home page

--- Bidster/Controllers/HomeController.cs
+++ Bidster/Controllers/HomeController.cs
@@ -8,16 +8,24 @@ using Bidster.Models;
 using Bidster.Data;
 using Bidster.Models.Home;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Bidster.Entities.Users;
 
 namespace Bidster.Controllers
 {
     public class HomeController : Controller
     {
         private readonly BidsterDbContext _dbContext;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(BidsterDbContext dbContext)
+        public HomeController(BidsterDbContext dbContext,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
             _dbContext = dbContext;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IActionResult> Index()
@@ -28,6 +36,41 @@ namespace Bidster.Controllers
                     && x.EndOn >= DateTime.Now)
                 .Select(x => ModelMapper.ToEventModel(x))
                 .ToListAsync();
+
+            var eventIds = model.Events.Select(x => x.Id);
+
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+
+                var userBiddingProductIds = await _dbContext.Bids.Include(x => x.Product)
+                    .Where(x => eventIds.Contains(x.Product.EventId)
+                        && x.UserId == user.Id)
+                    .Select(x => x.ProductId)
+                    .ToListAsync();
+
+                if (userBiddingProductIds.Any())
+                {
+                    model.UserBids = await (from bid in _dbContext.Bids.Include(x => x.Product)
+                                            where userBiddingProductIds.Contains(bid.ProductId)
+                                            group bid by bid.Product into grp
+                                            let maxBid = grp.Max(x => x.Amount)
+                                            select new BidSummaryModel
+                                            {
+                                                EventId = grp.Key.EventId,
+                                                EventName = model.Events.SingleOrDefault(x => x.Id == grp.Key.EventId).Name,
+                                                EventSlug = model.Events.SingleOrDefault(x => x.Id == grp.Key.EventId).Slug,
+                                                EventEndTime = model.Events.SingleOrDefault(x => x.Id == grp.Key.EventId).EndOn,
+                                                ProductId = grp.Key.Id,
+                                                ProductName = grp.Key.Name,
+                                                ProductSlug = grp.Key.Slug,
+                                                CurrentBidAmount = maxBid,
+                                                HighBigUserId = grp.First(x => x.Amount == maxBid).UserId,
+                                                IsWinning = grp.First(x => x.Amount == maxBid).UserId == user.Id
+                                            })
+                                            .ToListAsync();
+                }
+            }
 
             return View(model);
         }
--- Bidster/Models/Home/HomeViewModel.cs
+++ Bidster/Models/Home/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Bidster.Models.Events;
+using System;
 using System.Collections.Generic;
 
 namespace Bidster.Models.Home
@@ -6,5 +7,23 @@ namespace Bidster.Models.Home
     public class HomeViewModel
     {
         public List<EventModel> Events { get; set; }
+
+        public List<BidSummaryModel> UserBids { get; set; } = new List<BidSummaryModel>();
+    }
+
+    public class BidSummaryModel
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventSlug { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductSlug { get; set; }
+
+        public DateTime EventEndTime { get; set; }
+
+        public decimal CurrentBidAmount { get; set; }
+        public int HighBigUserId { get; set; }
+        public bool IsWinning { get; set; }
     }
 }
