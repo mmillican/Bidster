e13f5f36bb059b9736305d3512955a780d580835
Matt Millican <matt@mattmillican.com>
2019-02-07 23:50:11 -0600

    Better field labels

--- Bidster/Models/Events/EditEventViewModel.cs
+++ Bidster/Models/Events/EditEventViewModel.cs
@@ -10,13 +10,19 @@ namespace Bidster.Models.Events
         [Required, MaxLength(100)]
         public string Name { get; set; }
 
+        [Display(Name = "Event description")]
         public string Description { get; set; }
 
+        [Display(Name = "Start date/time")]
         public DateTime StartOn { get; set; }
+        [Display(Name = "End date/time")]
         public DateTime EndOn { get; set; }
+        [Display(Name = "Display date/time")]
         public DateTime DisplayOn { get; set; }
 
+        [Display(Name = "Hide bidder names")]
         public bool HideBidderNames { get; set; }
+        [Display(Name = "Default min bid amount")]
         public decimal? DefaultMinimumBidAmount { get; set; }
     }
 }
--- Bidster/Models/Products/EditProductViewModel.cs
+++ Bidster/Models/Products/EditProductViewModel.cs
@@ -12,12 +12,16 @@ namespace Bidster.Models.Products
         public string EventName { get; set; }
 
         public string Slug { get; set; }
+        [Display(Name = "Name")]
         [Required, MaxLength(100)]
         public string Name { get; set; }
 
+        [Display(Name = "Description")]
         public string Description { get; set; }
 
+        [Display(Name = "Starting price")]
         public decimal StartingPrice { get; set; }
+        [Display(Name = "Minimum bid amount")]
         public decimal MinimumBidAmount { get; set; }
 
         public bool HasBids { get; set; }
